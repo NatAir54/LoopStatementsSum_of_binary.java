56f4112a42b7497cf7bfcf7264a13c168b61318d
Nataliia Koval <ruba.fish1500@gmail.com>
2022-07-03 12:45:37 +0300

    Fix LoopStatements

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -1,9 +1,13 @@
 package com.epam.rd.autotasks;
-
-
 class LoopStatements {
     public static int task2(int n) {
-       //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+       int sum = 0;
+       String convert = Integer.toBinaryString(n);
+       int digit;
+       for (int i=0; i<convert.length(); i++) {
+           digit = convert.charAt(i) - '0';
+           if (digit==1) {
+               sum++;}
+           } return sum;
     }
 }
