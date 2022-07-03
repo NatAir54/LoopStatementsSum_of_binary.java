816c5c8c067a133278db8289941f61d21b466b43
Nataliia Koval <ruba.fish1500@gmail.com>
2022-07-03 12:49:56 +0300

    Fix LoopStatements

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -2,6 +2,7 @@ package com.epam.rd.autotasks;
 class LoopStatements {
     public static int task2(int n) {
        int sum = 0;
+       if (n>0) {
        String convert = Integer.toBinaryString(n);
        int digit;
        for (int i=0; i<convert.length(); i++) {
@@ -9,5 +10,7 @@ class LoopStatements {
            if (digit==1) {
                sum++;}
            } return sum;
+       } else {
+           throw new IllegalArgumentException();}
     }
 }
